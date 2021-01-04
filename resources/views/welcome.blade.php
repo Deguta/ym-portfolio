ffb7f76bcc0aa4e357df98a70307a495fbc8c4b1
Deguta <tech.ym1110@gmail.com>
2021-01-04 22:33:39 +0900

    Login-Function
    Login等の名前を日本語に変更

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -70,10 +70,10 @@
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
-                        <a href="{{ route('login') }}">Login</a>
+                        <a href="{{ route('login') }}">ログインはこちら</a>
 
                         @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
+                            <a href="{{ route('register') }}">初めての方はこちら</a>
                         @endif
                     @endauth
                 </div>
