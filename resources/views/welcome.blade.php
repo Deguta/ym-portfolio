3b442f40b05de9c56306152ed53bab272dd1e13f
Deguta <tech.ym1110@gmail.com>
2021-01-06 21:58:43 +0900

    MASTER UPDATE
    UPDATE

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,7 @@
         <!-- Styles -->
         <style>
             html, body {
-                background-color:lightgray;
+                background-color:lightyellow;
                 color: #636b6f;
                 font-family: 'Nunito', sans-serif;
                 font-weight: 200;
