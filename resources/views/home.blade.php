c7b5849e163211bd1da9422d0297d63f8f16acb9
Deguta <tech.ym1110@gmail.com>
2021-01-07 18:24:46 +0900

    Contact-Form-Function
    Update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    ログインが完了しました
                 </div>
             </div>
         </div>
