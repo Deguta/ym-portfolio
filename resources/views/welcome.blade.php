2611a4cda6eccf564759db4c8197e0dfb9f2354c
Deguta <tech.ym1110@gmail.com>
2021-01-15 14:53:56 +0900

    Top-Page
    トップページの骨組み完了

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,84 +3,77 @@
 <head>
     <meta charset="utf-8">
     <title>YM-PortFolio</title>
-    <link href="css/top_page.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="{{ asset('css/top_page.css') }}">
+    
 </head>
-    <header class="header-help-center-web">
-      <div class="header-help-center-web-inner clearfix">
-        <h1 class="l-left">
-          //<a href="https://www.mercari.com/jp/">
-            //<img src="//pcweb-assets.mercdn.net/assets/img/common/common/logo.svg?1309563043" alt="mercari" class="logo">
-          </a>
-        </h1>
-
-        <div class="l-right">
-                      <a href="https://www.mercari.com/jp/signup/" class="sp-header-btn btn-red">新規会員</a>
-<a href="https://www.mercari.com/jp/login/?login_callback=https%3A%2F%2Fwww.mercari.com%2Fjp%2Fhelp_center%2Fcategory%2F17%2F" class="sp-header-btn sp-header-signup">ログイン</a>
-                  </div>
-      </div>
-    </header>
-    
     <body>
-        <ul id="nav">
-            @if (Route::has('login'))
-                @auth
-                <li>
-                    <a href="{{ url('/home') }}">Home</a>
-                </li>
+        <div class="wrapper">
 
-                <li>@else
-                    a href="{{ route('login') }}">ログインはこちら</a>
-                </li>
+            <header>
+              <div class="top-page"> 
+                <img src="{{ asset("../images/3405998_m.jpg") }}" alt="トップページ" class="top-page-image">
+              </div>        
+            </header>
 
-                <li>
-                    @if (Route::has('register'))
-                        <a href="{{ route('register') }}">初めての方はこちら</a>
-                    @endif
-                </li>
-                @endauth
-            @endif
-        </ul>
 
-    
-    
-        <header class="header">
-            @if (Route::has('login'))
-                <nav class="login">
-                    <ul>
-                        <li>
-                            @auth
-                                <a href="{{ url('/home') }}">Home</a>
-                        </li>
-                        
-                        <li>
-                        @else
-                            <a href="{{ route('login') }}">ログインはこちら</a>
-                        </li>
-                        
-                        <li>
-                            @if (Route::has('register'))
-                                <a href="{{ route('register') }}">初めての方はこちら</a>
-                            @endif
-                        </li>
-                            @endauth
-                    </ul>
-                </nav>
-            @endif
-        </header>
-            
 
-    <div class="content">
-        <div class="title m-b-md">
-            <a href="{{ url('/contact/index') }}">YM-portfolio</a>
+            <div class="main">
+                <div class=about>
+                    <img src="{{ asset("../images/1647568_s.jpg") }}" alt="about-me" class="about-image">
+                    <div class="about-text">
+                        <div class="about-text-box">
+                            <a href="{{ url('/contact/index') }}">About Me</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class=portfolio>
+                    <img src="{{ asset("../images/2816548_s.jpg") }}" alt="portfolio-works" class="portfolio-image">
+                    <div class="portfolio-text">
+                        <div class="portfolio-text-box">
+                            <a href="{{ url('/contact/index') }}">製作物はこちら</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class=contact>
+                    <img src="{{ url("../images/4358663_s.jpg") }}" alt="お問い合わせ" class="contact-image">
+                    <div class="contact-text">
+                        <div class="contact-text-box">
+                            <a href="{{ url('/contact/index') }}">お問い合わせはこちら</a>
+                        </div>
+                    </div>
+                </div>
+
+
+                
+
+            </div>     
+                   
+  
+
+           
         </div>
-    </div>
-    
-    
-    
-    
-    
-    
-    
     </body>
 </html>
 
+
+
+{{--  @if (Route::has('login'))
+                <div class="header-left">ロゴ作成予定</div>
+
+                <div class="header-right">
+                        @auth
+                            <a href="{{ url('/home') }}">ログアウトはこちら</a>
+                        @else
+                            <div class=login>
+                                <a href="{{ route('login') }}">ログイン</a>
+                            </div>
+                            <div class=register>
+                            @if (Route::has('register'))
+                                <a href="{{ route('register') }}">新規会員はこちら</a>
+                            @endif
+                            </div>
+                        @endauth
+                    </div>
+                @endif  --}}
