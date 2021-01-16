875719e77d019b1dc69997af9d404b80de993bcd
Deguta <tech.ym1110@gmail.com>
2021-01-16 17:41:48 +0900

    About-Me
    フォントを調整(変更の可能性あり)
    母体の骨組み(変更の可能性あり)

--- app/Http/Controllers/AboutMeController.php
+++ app/Http/Controllers/AboutMeController.php
@@ -8,6 +8,7 @@ class AboutMeController extends Controller
 {
     public function index()
     {
+        
         //フォーム入力画ページのviewを表示
         return view('about_me.index');
     }
--- resources/views/about_me/index.blade.php
+++ resources/views/about_me/index.blade.php
@@ -1,10 +1,46 @@
-<!DOCTYPE HTML>
-<html>
+<!DOCTYPE html>
+<html lang="ja">
 <head>
-    <title>About</title>
+    <meta charset="utf-8">
+    <title>YM-PortFolio-About-Me</title>
+    
+    <link rel="stylesheet" href="{{ asset('css/about_me.css') }}">
+    <link href="{{asset('/assets/css/style.css')}}" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Caveat rel="stylesheet">
+    <link href="https://fonts.googleapis.com/earlyaccess/kokoro.css" rel="stylesheet">
+    <!-- httpsの場合 <link href="{{secure_asset('/assets/css/reset.css')}}" rel="stylesheet"> -->
 </head>
 <body>
-    <h1>About Me</h1>
+    <div class="wrapper">
+    <div id="main" class="inner">
+		
+		<!-- about -->
+		<section class="contBox cf">
+			<h2 class="ttl large">About</h2>
+			<p class="txtBox">大阪にてフリーランスのWeb制作をしております。<br>2006年よりアルバイトにて制作を始める。<br>2007年から5年間大阪市内の制作会社でデザイン・コーディングなどの業務を行う。<br>2012年よりフリーランスへ転向。</p>
+			<div class="fBox01 cf">
+				<div class="fBoxRight">
+					<dl class="companyProf cf">
+						<dt>名称 ：</dt><dd>UNDERLINE （アンダーライン）</dd>
+						<dt>所在地 ：</dt><dd>大阪市内</dd>
+						<dt>お問い合わせ ：</dt><dd>tokuda@u-d-l.jp</dd>
+						<dt>事業内容 ：</dt><dd>Web制作におけるディレクション、デザイン、HTML・CSSコーディング、スマートフォンサイト、CMS導入等</dd>
+					</dl>
+					<h2>徳田 優一　Tokuda Yuichi</h2>
+					<dl class="companyProf cf">
+						<dt>Facebook:</dt><dd><a href="https://www.facebook.com/yuichi.tokuda.5">yuichi.tokuda.5</a></dd>
+						<dt>Instagram:</dt><dd><a href="http://instagram.com/underline_t">underline_t</a></dd>
+					</dl>
+				</div>
+			</div>
+		</section>
+		
+		<!-- toTop -->
+		<p class="toTop"><a href="#top"><img src="https://u-d-l.jp/wp-content/themes/underline/common/icon_totop.gif" alt="" width="22" height="14"></a></p>
+		
+	</div>
+    </div>
+
+    
 </body>
-</html>
-<h1>about_meフォルダのindexファイル</h1>
+
