f6b011cf2d0a07f5b88d9f4591e5a9cc510d6177
Deguta <tech.ym1110@gmail.com>
2021-02-15 16:51:01 +0900

    MASTER UPDATE
    自己紹介サイトの骨組みを作成

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
   <body>
-    <class="container">
+    <div class="container">
       <!-- ナビゲーションバー -->
       <nav>
         <ul >
@@ -19,14 +19,16 @@
           <li><a href=”#pr”>自己PR</a></li>
         </ul>
       </nav>
+
+      
       <!-- 自己紹介 -->
       <div class="row no-gutters">
-        <div class="card mt-5 mx-auto  col-md-6 col-md-offset-6">
+        <div class="card mt-5 mx-auto  col-md-9 col-md-offset-3">
           <div class="h2 card-header text-left back-color-gradient">
             <section id="about-me">自己紹介</section>
           </div>
 
-          <img class="img-thumbnail col-md-8 col-md-offset-2 d-block mt-4 mx-auto" src="images/4317917_m.jpg" alt="自己紹介写真">
+          <img class="img-thumbnail col-md-10 col-md-offset-2 d-block mt-4 mx-auto" src="images/4317917_m.jpg" alt="自己紹介写真">
           <div class="card-body ">
             <p class="card-text text-center">
             採用者の皆様へ。<br>
@@ -38,22 +40,20 @@
         </div>
       </div>
 
-
       <div class="row no-gutters">
-        <div class="card mt-5 mx-auto  col-md-6 col-md-offset-6">
+        <div class="card mt-5 mx-auto  col-md-9 col-md-offset-3">
           <div class="h2 card-header text-left back-color-gradient">
-            <a id="career">経歴</a>
-        </div>
+            <section id="about-me">経歴</section>
+          </div>
 
-        <div class="card-text text-left mt-3 ml-5">
-          <dl>
-            <dt><h3>【学歴】</h3>
+          <div class="card-body mt-1">
+            <dt class="mt-3"><h3>【学歴】</h3>
               <dd>2003年4月〜2006年3月 京都成章高等学校</dd>
               <dd>2006年4月〜2010年3月 近畿大学 経営学部 経営学科 ITビジネスコース</dd>
             </dt>
             <div class="hidden_box">
               <label for="label1">学歴の詳細はこちら</label>
-              <input type="checkbox" id="label1"/>
+              <input type="checkbox" id="label1">
               <div class="hidden_show">
                 新聞会部と大学祭実行委員会に入部していました。芸能人やスポーツ選手が多く訪ねて くるのでそれのアポ取りや記事チェック等々の社会人に必要な基礎を学んでいました。
                 また、学祭の運営も行っていました。 アルバイトは蕎麦菓子と料亭で働いていました。そこで技術を身につけお客様に認めても らえる様になった。 大学ではITビジネスコースに入学しましたので基本的なマークアップ言語は勉強しまし
@@ -61,68 +61,173 @@
                 をインターネットで検索しをそれを形にできるから達成感を味わうことが面白かったから
                 です。
               </div>
-            </div>
-          </dl>
-
-          <dl>
-            <dt><h3>【職歴1】</h3>
-              <dd>2010年4月〜2015年9月 株式会社近商ストア</dd>
-            </dt>
-            <div class="hidden_box">
-              <label for="label2">職歴1の詳細はこちら</label>
-              <input type="checkbox" id="label2"/>
-              <div class="hidden_show">
-                [主な担当]
-                近鉄沿線の街の活性化し豊かな暮らしを支えるための食品を中心とした商品を販売。食品の加工・販売・接客を主とした仕事。
-
-                [成果]
-                2014年度 売上3億7800万円 予算達成率 105%　粗利 1800万
-                食卓を豊かにできるように旬の食材を提供したり、他部門と協力しメミュー提案をすることによって売場に工夫をした。その結果、前年の売上に対して5%程売上アップ。また、お客様が焼いてすぐに食べられるように工夫をして前年比10%アップ。企業内によるお造りコンテストで優勝。
-
+              <dt class="mt-3"><h3>【職歴1】</h3>
+                <dd>2010年4月〜2015年9月 株式会社近商ストア</dd>
+              </dt>
+              <div class="hidden_box">
+                <label for="label2">職歴1の詳細はこちら</label>
+                <input type="checkbox" id="label2"/>
+                <div class="hidden_show">
+                  [主な担当]
+                  近鉄沿線の街の活性化し豊かな暮らしを支えるための食品を中心とした商品を販売。食品の加工・販売・接客を主とした仕事。
+                  [成果]
+                  2014年度 売上3億7800万円 予算達成率 105%　粗利 1800万
+                  食卓を豊かにできるように旬の食材を提供したり、他部門と協力しメミュー提案をすることによって売場に工夫をした。その結果、前年の売上に対して5%程売上アップ。また、お客様が焼いてすぐに食べられるように工夫をして前年比10%アップ。企業内によるお造りコンテストで優勝。
+                </div>
+              </div>
+              <dt class="mt-3"><h3>【職歴2】</h3>
+                <dd>2015年10月〜2020年12月 株式会社サカノシタ</dd>
+              </dt>
+              <div class="hidden_box">
+                <label for="label3">職歴2の詳細はこちら</label>
+                <input type="checkbox" id="label3"/>
+                <div class="hidden_show">
+                  主な担当
+                  機械工具のルート営業
+                  【成果】
+                  2019年度 売上1億4000万円 粗利2100万円
+                  モノづくりのサポートをモットーに機械工具の販売、提案。
+                  昨年の12月には大型機械の受注を頂き１億円の売上達成。
+                  また、弊社の当月売上ランキング1位獲得。
+                </div>
               </div>
             </div>
-
-          <dl>
-            <dt><h3>【職歴2】</h3>
-              <dd>20015年10月〜2020年12月 株式会社サカノシタ</dd>
-            </dt>
-            <div class="hidden_box">
-              <label for="label3">職歴2の詳細はこちら</label>
-              <input type="checkbox" id="label3"/>
-              <div class="hidden_show">
-                主な担当
-                機械工具のルート営業
-
-                【成果】
-                2019年度 売上1億4000万円 粗利2100万円 
-                モノづくりのサポートをモットーに機械工具の販売、提案。
-                昨年の12月には大型機械の受注を頂き１億円の売上達成。
-                また、弊社の当月売上ランキング1位獲得。
-
-              </div>
-            </div>
-          </dl>
-
-
-
-
+          </div>
         </div>
       </div>
 
+      <div class="row no-gutters">
+        <div class="card mt-5 mx-auto  col-md-12">
+          <div class="h2 card-header text-left back-color-gradient">
+            <section id="about-me">制作物</section>
+          </div>
+        </div>
+      </div>
+      <div class="row no-gutters justify-content-around bg-white border">
+        <div class="col-12 col-sm-8 col-md-4">
+          <div class="card-body ">
+            <a href="#"><h2>制作物1</h2></a>
+            <img class="img-thumbnail col-md-8 col-md-offset-2 d-block mt-4 mx-auto" src="images/4317917_m.jpg" alt="自己紹介写真">
+            <p class="card-text text-left ">
+              採用者の皆様へ。<br>
+              大変お忙しい中、お時間を割いて頂きありがとうございます！<br>
+              兵庫県在住の南川 裕一(ミナミカワ ユウイチ)と申します。<br>
+              サーバーサイドエンジニアを目指して大阪府 兵庫県 京都府を中心に転職活動をしております。<br>
+            </p>
+          </div>
+        </div>
 
+        <div class="col-12 col-sm-8 col-md-4">
+          <div class="card-body ">
+            <a href="#"><h2>制作物2</h2></a>
+            <img class="img-thumbnail col-md-8 col-md-offset-2 d-block mt-4 mx-auto" src="images/4317917_m.jpg" alt="自己紹介写真">
+            <p class="card-text text-left ">
+              採用者の皆様へ。<br>
+              大変お忙しい中、お時間を割いて頂きありがとうございます！<br>
+              兵庫県在住の南川 裕一(ミナミカワ ユウイチ)と申します。<br>
+              サーバーサイドエンジニアを目指して大阪府 兵庫県 京都府を中心に転職活動をしております。<br>
+            </p>
+          </div>
+        </div>
+     </div>
 
+      <div class="row no-gutters">
+        <div class="card mt-5 mx-auto  col-md-9 col-md-offset-3">
+          <div class="h2 card-header text-left back-color-gradient">
+            <section id="about-me">保有資格 特技</section>
+          </div>
+          <div class="card-body ">
+            <p class="card-text left">
+            <ul id="ul-requirement">
+              <h3>資格一覧</h3>
+              <li>販売士検定3級</li>
+              <li>ファイナンシャルプランニング技能士3級</li>
+              <li>マイクロソフト認定資格（MOS）Word スペシャリスト</li>
+              <li>マイクロソフト認定資格（MOS）Excelスペシャリスト</li>
+              <li>普通自動車免許第一種</li>
+            </ul>
+            <ul id="ul-skill">
+              <h3>特技一覧</h3>
+              <li>ピアノ</li>
+              <li>合気道</li>
+              <li>魚料理</li>
+            </ul>    
+            </p>
+          </div>
+        </div>
+      </div>
 
+      <div class="row no-gutters">
+        <div class="card mt-5 mx-auto  col-md-9 col-md-offset-3">
+          <div class="h2 card-header text-left back-color-gradient">
+            <section id="about-me">自己PR</section>
+          </div>
+          <div class="card-body ">
+            <p class="card-text left">
+              <b>【柔軟な発想での課題解決力】コロナ不況による売上低迷を打破</b><br>
+                現職では機械工具のルート営業をしています。新型コロナにより設備投資がなくなり売上が低迷しました。
+                そこで、私は当時マスク無しで仕事ができず、生産活動を止めざる負えないお客様が多くいらっしゃいました。そこで、マスクを生産し供給することで、お客様が生産活動を再開できるのではないかと考えました。さっそく上司へ提案を行い、中国にも弊社の営業所はありますので至急マスクを生産できるところをチームメンバーで探しました。すると、1社がその案件に乗り出しマスクを生産することになりました。私が中心となって中国の営業所と連携し、価格・製造量・輸入のタイミングなど細かく調整を行い、企画から約1ヶ月でマスクを2万枚を日本に輸入することができました。
+                その結果、弊社のお客様にマスクを販売する事ができ大手企業様を中心に注文が殺到し、1日でマスクを完売させることができました。その結果、お客様はマスク不足を解消でき、会社でも3600万円の売上を上げることができました。
+                また、この功績により京都市と南丹市から災害時における物資供給を優先的に行うことができる協定を結部ことにも至りました。
+                入社後には、さまざまな観点からどのように課題を解決できるか検討し、社内やクライアントの課題解決に貢献したいと思います。
 
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="row no-gutters">
+        <div class="card mt-5 mx-auto  col-md-9 col-md-offset-3">
+          <div class="h2 card-header text-left back-color-gradient">
+            <section id="about-me">エンジニアを目指す理由</section>
+          </div>
+          <div class="card-body ">
+            <p class="card-text left">
+              採用者の皆様へ。<br>
+              大変お忙しい中、お時間を割いて頂きありがとうございます！<br>
+              兵庫県在住の南川 裕一(ミナミカワ ユウイチ)と申します。<br>
+              サーバーサイドエンジニアを目指して大阪府 兵庫県 京都府を中心に転職活動をしております。<br>
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="row no-gutters">
+        <div class="card mt-5 mx-auto  col-md-9 col-md-offset-3">
+          <div class="h2 card-header text-left back-color-gradient">
+            <section id="about-me">最後に</section>
+          </div>
+          <div class="card-body ">
+            <p class="card-text left">
+              ここまで読んでいただきありがとうございます。<br>
+              初学者でもありますので新しく勉強したことや理解したことはQiitaにて投稿しています!<br>
+              是非こちらも読んでいただいたらと思います。
+              <a href="https://qiita.com/yutarou">[私のQiitaはこちら]</a>
+            </p>
+            
+          </div>
+        </div>
+      </div>
+
+      <div class="contact">
+        <img src="{{ url("../images/4358663_s.jpg") }}" alt="お問い合わせ" class="contact-image">
+        <div class="contact-text">
+            <div class="contact-text-box">
+                <a href="{{ url('/contact/index') }}">Contact</a>
+            </div>
+        </div>
     </div>
+
+
+
+      
+      
+
+      
+    </div>
+
 
   </body>
 </html>
 
-                <!-- <div class="contact">
-                    <img src="{{ url("../images/4358663_s.jpg") }}" alt="お問い合わせ" class="contact-image">
-                    <div class="contact-text">
-                        <div class="contact-text-box">
-                            <a href="{{ url('/contact/index') }}">Contact</a>
-                        </div>
-                    </div>
-                </div> -->
+                
