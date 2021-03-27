2dd7ea3d04cccbbed8607e6506116b48b812b89a
Deguta <tech.ym1110@gmail.com>
2021-03-27 14:44:44 +0900

    MASTER UPDATE
    エンジニア志望理由 文章を修正

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,14 +14,17 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-    <
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/welcome.css') }}">
 
 </head>
+
 <body>
-    @yield('content')
+
+@yield('content')
+
 </body>
+
 </html>
